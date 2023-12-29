207fe07ab75579d57b17f0d0cbae2ac335a2eec9
Kimjongmin <rlajm1203@naver.com>
2023-12-30 01:43:23 +0900

    feat: studycomplete 엔티티 작성

---
+++ BE/src/main/java/com/econovation/rere/entity/StudyComplete.java
@@ -0,0 +1,34 @@
+package com.econovation.rere.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class StudyComplete {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer completeId;
+
+    // 어떤 회원이
+    @ManyToOne
+    private User user;
+
+    // 어떤 목차를 완료했는지를 나타내는
+    @ManyToOne
+    private Theme theme;
+
+    // 학습 완료 시간
+    @Column(columnDefinition = "TEXT")
+    private LocalDateTime completeDate;
+
+    // 학습 할 단계가 몇 단계인지
+    @Column
+    private Integer step;
+
+}
