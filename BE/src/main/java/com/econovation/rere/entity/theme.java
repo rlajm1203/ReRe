663d29dfccf66a78a91ba5f518c3e02620a06cb3
Kimjongmin <rlajm1203@naver.com>
2023-12-30 00:04:20 +0900

    feat: theme 엔티티 주석 수정

--- BE/src/main/java/com/econovation/rere/entity/theme.java
+++ BE/src/main/java/com/econovation/rere/entity/theme.java
@@ -2,8 +2,6 @@ package com.econovation.rere.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -25,10 +23,12 @@ public class theme {
     @Column(columnDefinition = "TEXT")
     private LocalDateTime updateDate;
 
-    @ManyToOne // 테이블의 참조 관계를 명시적으로 표시해주는 역할
-//    관계형 데이터의 외래키 라고 생각하면 편하다.
-//    또는 ManyToOne은 부모 자식 관계를 갖는 구조에서 사용한다.
-//    여기서 부모는 cardbook, 자식은 theme이다.
-    private cardbook cardbook;
+//  테이블의 참조 관계를 명시적으로 표시해주는 역할
+//  관계형 데이터의 외래키 라고 생각하면 편하다.
+//  또는 ManyToOne은 부모 자식 관계를 갖는 구조에서 사용한다.
+//  여기서 부모는 cardbook, 자식은 theme이다.
+//  cardbook : theme 는 1:N 관계
+   @ManyToOne
+   private cardbook cardbook;
 
 }
