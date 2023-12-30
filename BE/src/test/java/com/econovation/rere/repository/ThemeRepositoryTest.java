b889803ec46a0983675149c2ca6546efb417086c
Kimjongmin <rlajm1203@naver.com>
2023-12-31 00:01:16 +0900

    feat: ThemeRepository 테스트 코드 작성

--- BE/src/test/java/com/econovation/rere/repository/ThemeRepositoryTest.java
+++ BE/src/test/java/com/econovation/rere/repository/ThemeRepositoryTest.java
@@ -5,8 +5,10 @@ import com.econovation.rere.entity.Theme;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @SpringBootTest
 public class ThemeRepositoryTest {
@@ -22,11 +24,14 @@ public class ThemeRepositoryTest {
 
 //        theme과 cardbook은 의존 관계가 존재하는데,
 //        어떻게 설정하지?
+//        CardBookRepository의 findAll()을 이용해서
+//        CardBook Name과 일치하는 곳에 cardbook_cardbook_id 를 삽입하면 될 듯?
 //        theme.setCardbook();
 
         theme.setCreateDate(time);
         theme.setUpdateDate(time);
 
         this.repository.save(theme);
+
     }
 }
