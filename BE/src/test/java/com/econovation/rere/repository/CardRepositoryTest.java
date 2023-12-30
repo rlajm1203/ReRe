c429ae6dbf69317908a36ce9efcf80bcbbb9184b
Kimjongmin <rlajm1203@naver.com>
2023-12-31 00:00:15 +0900

    feat: CardRepository 테스트 코드 작성

--- BE/src/test/java/com/econovation/rere/repository/CardRepositoryTest.java
+++ BE/src/test/java/com/econovation/rere/repository/CardRepositoryTest.java
@@ -1,2 +1,32 @@
-package com.econovation.rere.repository;public class CardRepositoryTest {
+package com.econovation.rere.repository;
+
+import com.econovation.rere.entity.Card;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class CardRepositoryTest {
+
+    @Autowired
+    private CardRepository repository;
+
+    @Test
+    void testCardInsert(){
+        Card card = new Card();
+        LocalDateTime time = LocalDateTime.now();
+//        card 엔티티의 content 속성은 질문에 해당
+        card.setContent("2024년 1월 1일은 무슨 요일일까요?");
+        card.setAnswer("월요일");
+
+//        CardRepository의 findAll()을 사용해
+//        해당하는 Theme을 찾아서 삽입
+//        card.setTheme();
+        card.setCreateDate(time);
+        card.setUpdateDate(time);
+
+        repository.save(card);
+    }
 }
