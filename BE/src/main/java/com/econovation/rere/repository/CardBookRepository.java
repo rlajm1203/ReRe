222080535d1424595bac0941618c3c9954cd0b59
Kimjongmin <rlajm1203@naver.com>
2023-12-30 23:28:06 +0900

    feat: findByNameAndWriter() 메소드 추가

--- BE/src/main/java/com/econovation/rere/repository/CardBookRepository.java
+++ BE/src/main/java/com/econovation/rere/repository/CardBookRepository.java
@@ -1,5 +1,6 @@
 package com.econovation.rere.repository;
 
+import com.econovation.rere.entity.Card;
 import com.econovation.rere.entity.CardBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +9,6 @@ import java.util.Optional;
 public interface CardBookRepository extends JpaRepository<CardBook, Integer> {
 //    CardBookRepository는 레포지터리로 만들기 위해 JpaRepository 인터페이스를 상속하고,
 //    JpaRepository를 상속할 때는 제네릭 타입으로 CardBook(엔티티 타입), Integer(PK 타입)를 지정한다.
-
 
     /*
     CardBookRepository가 인터페이스이고 findByName은 구현도 하지 않았는데 어떻게 실행되지 라는 의문이 생기는데
@@ -20,9 +20,11 @@ public interface CardBookRepository extends JpaRepository<CardBook, Integer> {
     즉, JPA는 메소드 이름을 분석하여 쿼리를 생성하므로 "findBy + 엔티티 속성명"과 같은 메소드를 작성하면 해당 속성 값으로 데이터를 조회할 수 있는 것이다.
     */
 
-
 //    CardBook 엔티티의 Name 값으로 데이터를 조회하기 위한 메소드
     Optional<CardBook> findByName(String Name);
 
+//    CardBook의 제목과 작성자, 두 개의 조건으로 조회하기 위한 메소
+    Optional<CardBook> findByNameAndWriter(String Name, String Writer);
 
+    
 }
