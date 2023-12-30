c8fddc09c1493fffed5857bfce305bf9e0b0c31b
Kimjongmin <rlajm1203@naver.com>
2023-12-30 15:22:09 +0900

    feat: UserRepository 작성

---
+++ BE/src/main/java/com/econovation/rere/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.econovation.rere.repository;
+
+import com.econovation.rere.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}
