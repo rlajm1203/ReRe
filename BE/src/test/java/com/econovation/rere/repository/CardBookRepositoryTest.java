8fc024bd5642b02cfc6f78430a4a4f617564e36b
Kimjongmin <rlajm1203@naver.com>
2023-12-30 17:55:00 +0900

    feat: CardBookRepository 테스트 메소드 추가

--- BE/src/test/java/com/econovation/rere/repository/CardBookRepositoryTest.java
+++ BE/src/test/java/com/econovation/rere/repository/CardBookRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.econovation.rere.repository;
 
 import com.econovation.rere.entity.CardBook;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 public class CardBookRepositoryTest {
@@ -24,5 +26,20 @@ public class CardBookRepositoryTest {
 
 //      초기화한 cardBook DB에 insert
         this.repository.save(cardBook);
+    }
+
+    @Test
+    void testCardBookSelect(){
+        List<CardBook> all = this.repository.findAll();
+
+//        Assertions.assertEquals(기댓값, 실제값)
+//        아래 메소드는 수행한 테스트의 결과가 맞는지 비교하기 위한 메소드이다.
+//        기댓값과 실제값이 동일하지 않으면 테스트는 실패로 처리된다.
+        Assertions.assertEquals(1,all.size());
+
+        CardBook cardbook = all.get(0);
+        Assertions.assertEquals("정보처리기사",cardbook.getName());
+        Assertions.assertEquals(1, cardbook.getCardbookId());
+        
     }
 }
--- BE/src/test/java/com/econovation/rere/repository/UserRepositoryTest.java
+++ BE/src/test/java/com/econovation/rere/repository/UserRepositoryTest.java
@@ -16,18 +16,19 @@ public class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    void testJpa(){
+    void testUserInsert(){
 //        user1 생성
         User user1 = new User();
-
 //        user1에 대한 정보 입력
 //        각 Entity에 대한 정보 입력은 추후 생성자로 수정할 예정
-        user1.setNickname("KimJongMin");
-        user1.setLoginId("rere");
+        user1.setNickname("user01");
+        user1.setLoginId("rere1");
         user1.setPw("1234");
-        user1.setUserId(1);
-        this.userRepository.save(user1);
 
+//        user1.setUserId(3);
+//        User 엔티티의 Primary Key는 userId 인데, 작성하지 않아도 자동으로 값이 1씩 증가한다.
+
+        this.userRepository.save(user1);
     }
 
 }
