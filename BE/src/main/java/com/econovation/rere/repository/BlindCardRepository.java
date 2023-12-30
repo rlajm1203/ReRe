cfd4f8e47cee8a23be31d036863f800b7ebcb0b1
Kimjongmin <rlajm1203@naver.com>
2023-12-30 15:23:54 +0900

    feat: BlindCardRepository 작성

---
+++ BE/src/main/java/com/econovation/rere/repository/BlindCardRepository.java
@@ -0,0 +1,7 @@
+package com.econovation.rere.repository;
+
+import com.econovation.rere.entity.BlindCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlindCardRepository extends JpaRepository<BlindCard, Integer> {
+}
