c92e8850af125c2cd5c71d3a56fdd7fe1974a2ab
Kimjongmin <rlajm1203@naver.com>
2023-12-31 14:34:16 +0900

    feat: UserCardBookRepository findByAllUser 메소드 추가

--- BE/src/main/java/com/econovation/rere/repository/UserCardBookRepository.java
+++ BE/src/main/java/com/econovation/rere/repository/UserCardBookRepository.java
@@ -1,5 +1,6 @@
 package com.econovation.rere.repository;
 
+import com.econovation.rere.entity.User;
 import com.econovation.rere.entity.UserCardBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +9,6 @@ import java.util.List;
 public interface UserCardBookRepository extends JpaRepository<UserCardBook, Integer> {
 
 //    특정 사용자가 담은 카드북의 목록을 가져온다.
-    List<UserCardBook> findAllByUser(String nickname);
+    List<UserCardBook> findAllByUser(User user);
 
 }
