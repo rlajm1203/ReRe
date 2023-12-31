c1b7083e45654cec3e2e1feee71d08af1b7ff79a
Kimjongmin <rlajm1203@naver.com>
2024-01-01 01:45:28 +0900

    feat: add CardBookDTO

---
+++ BE/src/main/java/com/econovation/rere/domain/dto/CardBookDTO.java
@@ -0,0 +1,17 @@
+package com.econovation.rere.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardBookDTO {
+
+    private Integer cardBookId;
+
+
+}
