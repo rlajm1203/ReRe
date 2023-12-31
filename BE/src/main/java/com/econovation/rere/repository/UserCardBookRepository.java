ae372d0aab12a8f08bc05a827b4058460186479c
Kimjongmin <rlajm1203@naver.com>
2023-12-31 12:14:54 +0900

    feat: UserCardBook 엔티티 작성

---
+++ BE/src/main/java/com/econovation/rere/repository/UserCardBookRepository.java
@@ -0,0 +1,13 @@
+package com.econovation.rere.repository;
+
+import com.econovation.rere.entity.UserCardBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserCardBookRepository extends JpaRepository<UserCardBook, Integer> {
+
+//    특정 사용자가 담은 카드북의 목록을 가져온다.
+    List<UserCardBook> findAllByUser(String nickname);
+
+}
