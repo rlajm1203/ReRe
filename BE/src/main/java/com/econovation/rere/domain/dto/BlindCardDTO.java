05cbc2002eb60f24f02ec28121e78c5fd13ad1db
Kimjongmin <rlajm1203@naver.com>
2024-01-01 02:26:54 +0900

    feat: Add BlindCardDTO

---
+++ BE/src/main/java/com/econovation/rere/domain/dto/BlindCardDTO.java
@@ -0,0 +1,26 @@
+package com.econovation.rere.domain.dto;
+
+// 이 클래스는 실제로 전송되지 않는 데이터이지만, 만약을 위해 만들어 놓습니다.
+
+import com.econovation.rere.domain.entity.Card;
+import com.econovation.rere.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlindCardDTO {
+
+    private Integer blindId;
+
+//    이게 맞나..?
+    private User user;
+
+//    이게 맞나..?
+    private Card card;
+
+}
