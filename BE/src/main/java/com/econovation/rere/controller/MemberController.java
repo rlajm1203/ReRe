7687dd64c356debcdb4d8e264e4f2c0ffd27ddd5
myeongheon <axm0219@naver.com>
2024-01-02 00:09:51 +0900

    feat: Add MemberController-joinMember

--- BE/src/main/java/com/econovation/rere/controller/MemberController.java
+++ BE/src/main/java/com/econovation/rere/controller/MemberController.java
@@ -1,4 +1,32 @@
 package com.econovation.rere.controller;
 
+import com.econovation.rere.domain.dto.request.UserCreateRequestDTO;
+import com.econovation.rere.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원 가입
+    @PostMapping("signup")
+    public ResponseEntity<String> joinMember(@RequestBody @Valid UserCreateRequestDTO userCreateRequestDTO) {
+        memberService.join(userCreateRequestDTO);
+        log.info("info log={}", userCreateRequestDTO);
+        return new ResponseEntity<>("회원 가입이 완료되었습니다.", HttpStatus.CREATED);
+    }
+
+
 }
