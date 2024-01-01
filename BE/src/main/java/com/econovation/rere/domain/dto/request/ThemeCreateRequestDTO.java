4bcbbbcd740a4dcf2803e2e2c6443a707ce823c2
Kimjongmin <rlajm1203@naver.com>
2024-01-01 15:09:18 +0900

    feat: Add ThemeCreateRequestDTO

--- BE/src/main/java/com/econovation/rere/domain/dto/request/ThemeCreateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/ThemeCreateRequestDTO.java
@@ -4,7 +4,10 @@ import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
+import java.util.List;
 
+//  Theme을 새로 생성할 때 보내는 요청 정보를 담는 클래스
+//  Theme create와 Theme update는 요청 구조가 비슷하다.
 @Data
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,5 +25,9 @@ public class ThemeCreateRequestDTO {
 //    theme을 update 하는 시간이면 updateDate가 된다.
     @NotEmpty(message = "please write time now")
     private LocalDateTime timenow;
+
+//    theme 하나에 여러 카드가 존재하므로
+    @NotEmpty(message = "please write cards")
+    private List<CardCreateRequestDTO> cards;
 
 }
