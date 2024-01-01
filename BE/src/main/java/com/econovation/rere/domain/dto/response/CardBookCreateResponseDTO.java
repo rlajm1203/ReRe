b6f6a30f92ec585a298cb990c7dfc7404a2770ce
Kimjongmin <rlajm1203@naver.com>
2024-01-01 14:33:53 +0900

    feat: Add CardBookCreateResponseDTO

--- BE/src/main/java/com/econovation/rere/domain/dto/response/CardBookCreateResponseDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/response/CardBookCreateResponseDTO.java
@@ -1,2 +1,19 @@
-package com.econovation.rere.domain.dto.response;public class CardBookCreateResponseDTO {
+package com.econovation.rere.domain.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+//  메인 페이지에 카드북 하나 하나를 보여줄 때 필요한 데이터들을 담는 클래스
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CardBookCreateResponseDTO {
+
+    private Integer cardbookId;
+    private String name;
+    private String writer;
+    private LocalDateTime updateDate;
+
 }
