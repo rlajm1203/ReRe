64a875ec966c64edf5bea7186815be01cbc31774
Kimjongmin <rlajm1203@naver.com>
2024-01-01 13:37:13 +0900

    feat: Add NotEmpty Annotation

--- BE/src/main/java/com/econovation/rere/domain/dto/request/UserCreateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/UserCreateRequestDTO.java
@@ -1,22 +1,28 @@
 package com.econovation.rere.domain.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
+
+/*
+이 클래스는 HttpRequest에 담긴 데이터를 담는 클래스로
+UserCreate 요청은 UserId 값이 담겨서 오지 않는다.
+ */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class UserCreateRequestDTO {
 
-    private Integer userId;
-
+//  UserDTO 데이터를 받을 때는 UserId 값을 받지 않는다.
+    @NotEmpty(message ="Please write ID")
     private String loginId;
 
+    @NotEmpty(message ="Please write PassWord")
     private String pw;
 
+    @NotEmpty(message ="Please write NickName")
     private String nickname;
 
 }
