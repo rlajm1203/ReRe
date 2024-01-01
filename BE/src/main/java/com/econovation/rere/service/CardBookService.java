1421180a0c3827e3f947a1cc72e16bbf86d45440
Kimjongmin <rlajm1203@naver.com>
2024-01-01 19:27:15 +0900

    feat: Add CardBookService

--- BE/src/main/java/com/econovation/rere/service/CardBookService.java
+++ BE/src/main/java/com/econovation/rere/service/CardBookService.java
@@ -15,9 +15,11 @@ public class CardBookService {
     private final CardBookRepository cardBookRepository;
 
     public CardBook create(String name, String writer, LocalDateTime createDate){
+        
         CardBook cardBook = new CardBook(name, writer, createDate);
         cardBookRepository.save(cardBook);
 
         return cardBook;
+
     }
 }
