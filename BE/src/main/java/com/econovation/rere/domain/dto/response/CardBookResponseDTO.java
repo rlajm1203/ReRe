eb56e054acf7745ee17ede3c5c02d15f21be4ed2
Kimjongmin <rlajm1203@naver.com>
2024-01-01 15:18:28 +0900

    docs: Add comment on CardBookResponseDTO

--- BE/src/main/java/com/econovation/rere/domain/dto/response/CardBookResponseDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/response/CardBookResponseDTO.java
@@ -5,6 +5,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 //  메인 페이지에 카드북 하나 하나를 보여줄 때 필요한 데이터들을 담는 클래스
+// Create, Update 종류로 구분하지 않은 이유는 어차피 Response로 보낼 형식이 똑같기 때문이다.
 @Data
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
