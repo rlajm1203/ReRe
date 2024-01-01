c0837fb1256da47fa7ca2f235a31ed3221ade928
Kimjongmin <rlajm1203@naver.com>
2024-01-01 23:24:41 +0900

    refactor: remove LocalDateTime field

--- BE/src/main/java/com/econovation/rere/domain/dto/request/ThemeCreateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/ThemeCreateRequestDTO.java
@@ -23,8 +23,8 @@ public class ThemeCreateRequestDTO {
 //    Request 요청에는 하나의 시간 정보가 담겨있다.
 //    따라서 이 정보가 theme을 create 하는 시간이면 createDate
 //    theme을 update 하는 시간이면 updateDate가 된다.
-    @NotEmpty(message = "please write time now")
-    private LocalDateTime timenow;
+//    @NotEmpty(message = "please write time now")
+//    private LocalDateTime timenow;
 
 //    theme 하나에 여러 카드가 존재하므로
     @NotEmpty(message = "please write cards")
