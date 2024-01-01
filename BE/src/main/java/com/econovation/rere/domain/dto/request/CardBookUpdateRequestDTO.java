5a7053582ff781ae1dd6f7cbf686ab38e117e8bd
Kimjongmin <rlajm1203@naver.com>
2024-01-01 23:24:12 +0900

    refactor: remove LocalDateTime field

--- BE/src/main/java/com/econovation/rere/domain/dto/request/CardBookUpdateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/CardBookUpdateRequestDTO.java
@@ -16,7 +16,10 @@ public class CardBookUpdateRequestDTO {
     @NotEmpty(message = "please write new name")
     private String name;
 
-    @NotEmpty(message = "please write time now")
-    private LocalDateTime timenow;
+    //    시간 정보는 프론트에서 전송 시점을 기준으로 하는 게 아니라
+//    백엔드가 요청을 받은 시점을 기준으로 해야하는 듯
+
+//    @NotEmpty(message = "please write time now")
+//    private LocalDateTime timenow;
 
 }
