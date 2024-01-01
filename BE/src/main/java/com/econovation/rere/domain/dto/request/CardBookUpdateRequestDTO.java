fa55a34c63f8ac7ba9edcf8e422db5f8f1c9fae0
Kimjongmin <rlajm1203@naver.com>
2024-01-02 02:49:07 +0900

    refactor: modify field

--- BE/src/main/java/com/econovation/rere/domain/dto/request/CardBookUpdateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/CardBookUpdateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.econovation.rere.domain.dto.request;
 
+import com.econovation.rere.domain.entity.CardBook;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
@@ -15,6 +16,9 @@ public class CardBookUpdateRequestDTO {
 
     @NotEmpty(message = "please write new name")
     private String name;
+
+    @NotEmpty(message = "please write cardbookId")
+    private Integer cardbookId;
 
     //    시간 정보는 프론트에서 전송 시점을 기준으로 하는 게 아니라
 //    백엔드가 요청을 받은 시점을 기준으로 해야하는 듯
