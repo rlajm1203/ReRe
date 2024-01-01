6a44e695a3e339a1fea20142aa11e7070e30c874
myeongheon <axm0219@naver.com>
2024-01-02 00:17:27 +0900

    feat:rename MemberController to UserController

--- BE/src/main/java/com/econovation/rere/controller/UserController.java
+++ BE/src/main/java/com/econovation/rere/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.econovation.rere.controller;
 
 import com.econovation.rere.domain.dto.request.UserCreateRequestDTO;
-import com.econovation.rere.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +14,14 @@ import javax.validation.Valid;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-public class MemberController {
+public class UserController {
 
-    private final MemberService memberService;
+    private final UserService userService;
 
     // 회원 가입
     @PostMapping("signup")
     public ResponseEntity<String> joinMember(@RequestBody @Valid UserCreateRequestDTO userCreateRequestDTO) {
-        memberService.join(userCreateRequestDTO);
+        userService.join(userCreateRequestDTO);
         log.info("info log={}", userCreateRequestDTO);
         return new ResponseEntity<>("회원 가입이 완료되었습니다.", HttpStatus.CREATED);
     }
