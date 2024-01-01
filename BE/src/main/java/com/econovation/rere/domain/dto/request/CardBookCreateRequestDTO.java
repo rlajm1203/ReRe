18782ea7a5e1e1ed7ef4fc1eeee91ad90577ba6f
Kimjongmin <rlajm1203@naver.com>
2024-01-02 02:44:36 +0900

    feat: add toEntity method

--- BE/src/main/java/com/econovation/rere/domain/dto/request/CardBookCreateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/CardBookCreateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.econovation.rere.domain.dto.request;
 
+import com.econovation.rere.domain.entity.CardBook;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
@@ -14,13 +15,19 @@ public class CardBookCreateRequestDTO {
     @NotEmpty(message = "please write cardbook name")
     private String name;
 
-    @NotEmpty(message = "please write cardbook writer")
-    private Integer userId;
-
 //    시간 정보는 프론트에서 전송 시점을 기준으로 하는 게 아니라
 //    백엔드가 요청을 받은 시점을 기준으로 해야하는 듯
 
 //    @NotEmpty(message = "please write time now")
 //    private LocalDateTime timenow;
+
+    public CardBook toEntity(String writer, LocalDateTime timenow){
+        return CardBook.builder()
+                .name(name)
+                .writer(writer)
+                .createDate(timenow)
+                .updateDate(timenow)
+                .build();
+    }
 
 }
