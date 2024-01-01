e512c083ff3a0d4099676cb3cf8c112d72b63ae1
Kimjongmin <rlajm1203@naver.com>
2024-01-01 15:14:38 +0900

    refactor: modify field name

--- BE/src/main/java/com/econovation/rere/domain/dto/response/CardBookCreateResponseDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/response/CardBookCreateResponseDTO.java
@@ -14,6 +14,6 @@ public class CardBookCreateResponseDTO {
     private Integer cardbookId;
     private String name;
     private String writer;
-    private LocalDateTime updateDate;
+    private LocalDateTime timenow;
 
 }
