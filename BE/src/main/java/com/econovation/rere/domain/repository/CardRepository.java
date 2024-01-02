b20b9e5639dc9cc4a2d5576cbad764c7f6bc55a7
Kimjongmin <rlajm1203@naver.com>
2024-01-02 16:29:52 +0900

    feat: add findAllByTheme method

--- BE/src/main/java/com/econovation/rere/domain/repository/CardRepository.java
+++ BE/src/main/java/com/econovation/rere/domain/repository/CardRepository.java
@@ -1,7 +1,14 @@
 package com.econovation.rere.domain.repository;
 
 import com.econovation.rere.domain.entity.Card;
+import com.econovation.rere.domain.entity.Theme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardRepository extends JpaRepository<Card, Integer> {
+
+//    Theme에 딸린 카드들 가져오기
+    public List<Card> findAllByTheme(Theme theme);
+
 }
