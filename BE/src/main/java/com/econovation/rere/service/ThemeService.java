d91cb89ca50e859d3a1be709246c6ce4eb6a0b2b
Kimjongmin <rlajm1203@naver.com>
2024-01-02 20:59:46 +0900

    refactor: rename 'removeTheme' -> 'remove'

--- BE/src/main/java/com/econovation/rere/service/ThemeService.java
+++ BE/src/main/java/com/econovation/rere/service/ThemeService.java
@@ -32,6 +32,7 @@ public class ThemeService {
     private final CardService cardService;
 
 //    생성
+//    생성,수정할 때만 목차 정보와 카드 정보를 동시에 받는다.
     public ThemeResponseDTO register(ThemeCreateRequestDTO themeCreateRequestDTO, Integer cardbookId){
         LocalDateTime timenow = LocalDateTime.now();
 
@@ -48,13 +49,13 @@ public class ThemeService {
 
         // Theme에 속한 Card들을 연관관계와 함께 저장
         for(CardCreateRequestDTO dto : themeCreateRequestDTO.getCards()){
-            log.info(cardService.register(dto, theme, timenow)+"");
+            cardService.register(dto, theme, timenow);
         }
-
         return toThemeResponseDTO(theme);
     }
 
 //    수정
+//    생성,수정할 때만 목차 정보와 카드 정보를 동시에 받는다.
     public ThemeResponseDTO modify(ThemeUpdateRequestDTO themeUpdateRequestDTO, Integer themeId){
         LocalDateTime timenow = LocalDateTime.now();
         Theme theme = themeRepository.findById(themeId).orElseThrow(()->new IllegalArgumentException("존재하지 않는 목차입니다."));
@@ -63,12 +64,18 @@ public class ThemeService {
         theme.setCardList(cardService.UpdateDTOStoCardEntities(themeUpdateRequestDTO.getCards(),timenow));
         theme = themeRepository.save(theme);
 
+//        카드 수정이 목차 수정과 동시에 이루어진다.
+        for(CardUpdateRequestDTO dto : themeUpdateRequestDTO.getCards()){
+            cardService.modify(dto,themeId,timenow);
+        }
+
         return toThemeResponseDTO(theme);
+
     }
 
 //    삭제
-    public boolean removeTheme(Integer cardbookId, Integer themeId){
-        Optional<Theme> ot = themeRepository.findByCardbookAndThemeId(cardbookId, themeId);
+    public boolean remove(Integer themeId){
+        Optional<Theme> ot = themeRepository.findById(themeId);
         if(themeRepository.deleteByThemeId(themeId)==1) return true;
         else return false;
     }
@@ -79,13 +86,14 @@ public class ThemeService {
         return themeRepository.findAllByCardbook(cardBook);
     }
 
+
+//    내부 메소드
     private ThemeResponseDTO toThemeResponseDTO(Theme theme){
         return ThemeResponseDTO.builder()
                 .themeId(theme.getThemeId())
                 .name(theme.getName())
                 .build();
     }
-
     private List<ThemeResponseDTO> toThemeResponseDTOS(List<Theme> themes){
         List<ThemeResponseDTO> themeResponseDTOS = new ArrayList<>();
         int length = themes.size();
@@ -96,5 +104,4 @@ public class ThemeService {
 
         return themeResponseDTOS;
     }
-
 }
