d4ef7a57ec6a5ef98a37f3515f70daf912f712f8
myeongheon <axm0219@naver.com>
2024-01-02 18:16:36 +0900

    feat:Add ApiResult

---
+++ BE/src/main/java/com/econovation/rere/apiresponse/ApiResult.java
@@ -0,0 +1,16 @@
+package com.econovation.rere.apiresponse;
+
+import lombok.Getter;
+
+@Getter
+public class ApiResult<T> {
+    private final boolean success;
+    private final T response;
+    private final ApiError apiError;
+
+    public ApiResult(boolean success, T response, ApiError apiError) {
+        this.success = success;
+        this.response = response;
+        this.apiError = apiError;
+    }
+}
