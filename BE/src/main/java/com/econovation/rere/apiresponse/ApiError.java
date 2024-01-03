61b3ca3d3d5758b4cd19606d775e3e29bf1515ba
Kimjongmin <rlajm1203@naver.com>
2024-01-03 20:16:58 +0900

    refactor: remove message field

--- BE/src/main/java/com/econovation/rere/apiresponse/ApiError.java
+++ BE/src/main/java/com/econovation/rere/apiresponse/ApiError.java
@@ -4,11 +4,9 @@ import lombok.Getter;
 
 @Getter
 public class ApiError {
-    private final String message;
     private final int status;
 
-    public ApiError(String message, int status) {
-        this.message = message;
+    public ApiError(int status) {
         this.status = status;
     }
 }
