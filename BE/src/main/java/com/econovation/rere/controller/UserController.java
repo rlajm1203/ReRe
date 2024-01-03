c7ee94059f044aead07d825b283a43aec4f46a66
myeongheon <axm0219@naver.com>
2024-01-03 13:17:47 +0900

    feat:Modify controller and add check-nickname

--- BE/src/main/java/com/econovation/rere/controller/UserController.java
+++ BE/src/main/java/com/econovation/rere/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.econovation.rere.controller;
 
+import com.econovation.rere.apiresponse.ApiError;
+import com.econovation.rere.apiresponse.ApiResult;
+import com.econovation.rere.apiresponse.ApiUtils;
 import com.econovation.rere.domain.dto.request.UserCreateRequestDTO;
 import com.econovation.rere.domain.dto.request.UserLoginIdRequestDTO;
+import com.econovation.rere.domain.dto.request.UserNicknameRequestDTO;
 import com.econovation.rere.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,21 +23,30 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("check-login")
-    public ResponseEntity<String> checkUserLoginId(@RequestBody @Valid UserLoginIdRequestDTO userLoginIdRequestDTO) {
+    public ApiResult<?> checkUserLoginId(@RequestBody @Valid UserLoginIdRequestDTO userLoginIdRequestDTO) {
         if (userService.checkLoginId(userLoginIdRequestDTO)) {
-            return new ResponseEntity<>("이미 존재하는 아이디 입니다.", HttpStatus.BAD_REQUEST);
+            return ApiUtils.error("중복된 아이디입니다", HttpStatus.BAD_REQUEST);
         } else {
-            return new ResponseEntity<>("사용가능한 아이디 입니다.", HttpStatus.OK);
+            return ApiUtils.success("사용 가능한 아이디입니다");
         }
-
     }
 
-    // 회원 가입 요청 처리
-    @PostMapping("signup")
-    public ResponseEntity<String> joinMember(@RequestBody @Valid UserCreateRequestDTO userCreateRequestDTO) {
+    @PostMapping("/check-nickname")
+    public ApiResult<?> checkUserNickname(@RequestBody @Valid UserNicknameRequestDTO userNicknameRequestDTO) {
+        if (userService.checkNickname(userNicknameRequestDTO)) {
+            return ApiUtils.error("중복된 닉네임입니다", HttpStatus.BAD_REQUEST);
+        } else {
+            return ApiUtils.success("사용 가능한 닉네임입니다");
+        }
+    }
+
+
+
+    @PostMapping("/signup")
+    public ApiResult<?> joinMember(@RequestBody @Valid UserCreateRequestDTO userCreateRequestDTO) {
         userService.join(userCreateRequestDTO);
-        log.info("info log={}", userCreateRequestDTO);
-        return new ResponseEntity<>("회원 가입이 완료되었습니다.", HttpStatus.CREATED);
+        return ApiUtils.success("회원가입 성공");
+
     }
 
 
