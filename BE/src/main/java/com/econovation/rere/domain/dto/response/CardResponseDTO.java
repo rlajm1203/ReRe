38ef2f7088c95f4836fc0691bf86895b929cbb38
Kimjongmin <rlajm1203@naver.com>
2024-01-07 23:58:29 +0900

    feat: add toCardResponseDTO, toCardResponseDTOS method

--- BE/src/main/java/com/econovation/rere/domain/dto/response/CardResponseDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/response/CardResponseDTO.java
@@ -1,7 +1,14 @@
 package com.econovation.rere.domain.dto.response;
 
 
+import com.econovation.rere.domain.dto.request.CardCreateRequestDTO;
+import com.econovation.rere.domain.dto.request.CardUpdateRequestDTO;
+import com.econovation.rere.domain.entity.Card;
 import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 //  메인 페이지에 카드북 하나 하나를 보여줄 때 필요한 데이터들을 담는 클래스
 //  Create, Update 종류로 구분하지 않은 이유는 어차피 Response로 보낼 형식이 똑같기 때문이다.
@@ -14,5 +21,21 @@ public class CardResponseDTO {
     private Integer cardId;
     private String content;
     private String answer;
+
+    public static CardResponseDTO toCardResponseDTO(Card card){
+        return CardResponseDTO.builder()
+                .cardId(card.getCardId())
+                .content(card.getContent())
+                .answer(card.getAnswer())
+                .build();
+    }
+
+    public static List<CardResponseDTO> toCardResponseDTOS(List<Card> cards){
+        List<CardResponseDTO> cardResponseDTOS = new ArrayList<>();
+
+        for(Card card : cards) cardResponseDTOS.add(toCardResponseDTO(card));
+
+        return cardResponseDTOS;
+    }
 
 }
