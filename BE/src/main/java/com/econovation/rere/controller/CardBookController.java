88674ace6cbf430ea28567b5190ca9daa0c192ab
Kimjongmin <rlajm1203@naver.com>
2024-01-07 19:56:46 +0900

    feat: add chooseCardBook, unchooseCardBook method

--- BE/src/main/java/com/econovation/rere/controller/CardBookController.java
+++ BE/src/main/java/com/econovation/rere/controller/CardBookController.java
@@ -4,10 +4,14 @@ import com.econovation.rere.apiresponse.ApiResult;
 import com.econovation.rere.apiresponse.ApiUtils;
 import com.econovation.rere.domain.dto.request.*;
 import com.econovation.rere.domain.dto.response.CardBookResponseDTO;
+import com.econovation.rere.domain.dto.response.MainPageResponseDTO;
+import com.econovation.rere.domain.dto.response.UserCardBookResponseDTO;
 import com.econovation.rere.service.CardBookService;
 import com.econovation.rere.service.ThemeService;
+import com.econovation.rere.service.UserCardBookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +26,7 @@ public class CardBookController {
 
     private final CardBookService cardBookService;
     private final ThemeService themeService;
+    private final UserCardBookService userCardBookService;
 
 //    생성
     @PostMapping("/cardbook")
@@ -60,11 +65,28 @@ public class CardBookController {
 
 //    메인 페이지 카드북 조회
     @GetMapping("/cardbooks")
-    public ApiResult<List<CardBookResponseDTO>> mainpageCardBook(){
-        List<CardBookResponseDTO> cardBookResponseDTOS = cardBookService.getDefaultCardbook();
-        return ApiUtils.success(cardBookResponseDTOS, "메인 페이지 카드북 목록입니다.");
+    public ApiResult<MainPageResponseDTO> mainpageCardBook(){
+        List<CardBookResponseDTO> defaultCardbook = cardBookService.getDefaultCardbook();
+        List<CardBookResponseDTO> myCardbook = cardBookService.getMyCardbook(1);
+
+        MainPageResponseDTO mainPageResponseDTO = MainPageResponseDTO.builder()
+                .originCardbooks(defaultCardbook)
+                .myCardbooks(myCardbook)
+                .build();
+
+        return ApiUtils.success(mainPageResponseDTO, "메인 페이지 카드북 목록입니다.");
     }
 
+//    사용자가 카드북 담기
     @PostMapping("/cardbook/{cardbookId}")
-    public ApiResult<List<CardBookResponseDTO>>
+    public ApiResult<UserCardBookResponseDTO> chooseCardBook(@PathVariable("cardbookId") Integer cardbookId){
+        UserCardBookResponseDTO userCardBookResponseDTO = userCardBookService.choose(1, cardbookId);
+        return ApiUtils.success(userCardBookResponseDTO, "카드북을 담았습니다.");
+    }
+
+//    사용자가 카드북 다시 반환
+    @DeleteMapping("/cardbook/{cardbookId}")
+    public ApiResult<UserCardBookResponseDTO> unchooseCardBook(@PathVariable("cardbookId") Integer cardbookId){
+        return ApiUtils.success(userCardBookService.unchoose(1, cardbookId), "카드북을 제외하였습니다.");
+    }
 }
