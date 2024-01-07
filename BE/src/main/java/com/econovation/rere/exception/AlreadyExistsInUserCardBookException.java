da3cd81ef09fd68c31d891e5811106b1301fb53c
Kimjongmin <rlajm1203@naver.com>
2024-01-07 21:25:15 +0900

    refactor: add Default Constructor

--- BE/src/main/java/com/econovation/rere/exception/AlreadyExistsInUserCardBookException.java
+++ BE/src/main/java/com/econovation/rere/exception/AlreadyExistsInUserCardBookException.java
@@ -1,6 +1,8 @@
 package com.econovation.rere.exception;
 
 public class AlreadyExistsInUserCardBookException extends RuntimeException{
+
+    public AlreadyExistsInUserCardBookException(){ this("해당 카드북을 이미 담았습니다.");}
     public AlreadyExistsInUserCardBookException(String message){
         super(message);
     }
