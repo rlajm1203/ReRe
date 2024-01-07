aa54f1a11162b6fc0dc1de43f7e9001a1f226132
Kimjongmin <rlajm1203@naver.com>
2024-01-07 18:53:30 +0900

    refactor: add choose, unchoose method

--- BE/src/main/java/com/econovation/rere/service/UserCardBookService.java
+++ BE/src/main/java/com/econovation/rere/service/UserCardBookService.java
@@ -1,8 +1,6 @@
 package com.econovation.rere.service;
 
-import com.econovation.rere.domain.entity.Card;
-import com.econovation.rere.domain.entity.CardBook;
-import com.econovation.rere.domain.entity.User;
+import com.econovation.rere.domain.dto.response.UserCardBookResponseDTO;
 import com.econovation.rere.domain.entity.UserCardBook;
 import com.econovation.rere.domain.repository.CardBookRepository;
 import com.econovation.rere.domain.repository.UserCardBookRepository;
@@ -10,6 +8,8 @@ import com.econovation.rere.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
@@ -22,20 +22,25 @@ public class UserCardBookService {
 
 //    사용자가 특정 카드북을 담았을 때
     @Transactional(readOnly = false)
-    public UserCardBook choose(Integer userId, Integer cardbookId){
+    public UserCardBookResponseDTO choose(Integer userId, Integer cardbookId){
+        // userId -> Session에서 꺼내서 사용
+        // cardbookId -> 요청에서 찾아서 사용
         UserCardBook userCardBook = UserCardBook.builder()
                 .user(userRepository.findById(userId).orElseThrow(()->new IllegalArgumentException("존재하지 않는 사용자 입니다.")))
                 .cardbook(cardBookRepository.findById(cardbookId).orElseThrow(()->new IllegalArgumentException("존재하지 않는 카드북입니다.")))
+                .chooseDate(LocalDateTime.now())
                 .build();
-        return userCardBookRepository.save(userCardBook);
+        userCardBookRepository.save(userCardBook);
+
+        return UserCardBookResponseDTO.toUserCardBookResponse(userCardBook);
     }
 
 //    담은 카드북을 담기 취소하는 것,
     @Transactional(readOnly = false)
     public void unchoose(Integer userId, Integer cardbookId){
-        CardBook cardbook = cardBookRepository.findById(cardbookId).orElseThrow(()->new IllegalArgumentException("존재하지 않는 카드북입니다."));
-        User user = userRepository.findById(userId).orElseThrow(()->new IllegalArgumentException("존재하지 않는 사용자입니다."));
-
-//        userCardBookRepository.deleteUserCardBookByCardbookAndUser(cardbook, user);
+        userCardBookRepository.findByCardbookAndUser(
+                cardBookRepository.findById(cardbookId).orElseThrow(),
+                userRepository.findById(userId).orElseThrow()
+        ).orElseThrow().setDeleted(true);
     }
 }
