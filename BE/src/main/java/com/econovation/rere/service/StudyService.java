ac888445e592874d2e643e0f2f337ed3e4b61652
Kimjongmin <rlajm1203@naver.com>
2024-01-09 03:44:51 +0900

    feat: studyComplete, studyStepCheck method

--- BE/src/main/java/com/econovation/rere/service/StudyService.java
+++ BE/src/main/java/com/econovation/rere/service/StudyService.java
@@ -3,10 +3,7 @@ package com.econovation.rere.service;
 import com.econovation.rere.domain.dto.response.StudyCompleteResponseDTO;
 import com.econovation.rere.domain.entity.*;
 import com.econovation.rere.domain.repository.*;
-import com.econovation.rere.exception.CardBookNotFoundException;
-import com.econovation.rere.exception.ThemeNotFoundException;
-import com.econovation.rere.exception.UserCardBookNotFoundException;
-import com.econovation.rere.exception.UserNotFoundException;
+import com.econovation.rere.exception.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -27,24 +24,41 @@ public class StudyService {
     private final CardBookRepository cardBookRepository;
 
     @Transactional(readOnly = false)
-    public StudyCompleteResponseDTO firstStudyComplete(Integer cardbookId, Integer themeId, Integer userId){
-        LocalDateTime timenow = LocalDateTime.now();
+    public StudyCompleteResponseDTO studyComplete(Integer cardbookId, Integer themeId, Integer userId){
         User user = userRepository.findById(userId).orElseThrow(()-> new UserNotFoundException());
         Theme theme = themeRepository.findById(themeId).orElseThrow(()->new ThemeNotFoundException());
         CardBook cardBook = cardBookRepository.findById(cardbookId).orElseThrow(()-> new CardBookNotFoundException());
 
         UserCardBook userCardBook = userCardBookRepository.findByCardbookAndUser(cardBook, user).orElseThrow(()-> new UserCardBookNotFoundException());
 
-        StudyComplete studyComplete = studyCompleteRepository.save(
-                StudyComplete.builder()
-                        .completeDate(timenow)
-                        .userCardBook(userCardBook)
-                        .theme(theme)
-                        .step(0)
-                        .build()
-        );
+        return StudyCompleteResponseDTO.toStudyCompleteResponseDTO(studyCompleteRepository.save(studyStepCheck(cardbookId, themeId, userId)), user.getNickname());
+    }
 
+//    사용자가 현재 학습한 목차가 몇 단계인지 체크하는 메소드
+    private StudyComplete studyStepCheck(Integer cardbookId, Integer themeId, Integer userId){
 
-        return StudyCompleteResponseDTO.toStudyCompleteResponseDTO(studyComplete, user.getNickname());
+        LocalDateTime timenow = LocalDateTime.now();
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
+        CardBook cardBook = cardBookRepository.findById(cardbookId).orElseThrow(() -> new CardBookNotFoundException());
+        Theme theme = themeRepository.findById(themeId).orElseThrow(() -> new ThemeNotFoundException());
+
+        UserCardBook userCardBook = userCardBookRepository.findByCardbookAndUser(cardBook, user).orElseThrow(() -> new UserCardBookNotFoundException());
+
+        if(studyCompleteRepository.existsByUserCardBookAndTheme(userCardBook, theme)){
+            StudyComplete studyComplete = studyCompleteRepository.findByUserCardBookAndTheme(userCardBook, theme).orElseThrow(()->new StudyCompleteNotFoundException());
+            studyComplete.setStep((studyComplete.getStep()+1)%5);
+            studyComplete.setCount(studyComplete.getCount()+1);
+            return studyComplete;
+        }
+        else{
+            return StudyComplete.builder()
+                    .step(0)
+                    .count(0)
+                    .completeDate(timenow)
+                    .theme(theme)
+                    .userCardBook(userCardBook)
+                    .build();
+        }
     }
 }
