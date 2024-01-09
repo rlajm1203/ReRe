df293cdcb938dd8bd86979799ed859d880959955
myeongheon <axm0219@naver.com>
2024-01-09 19:51:28 +0900

    feat:Modify UserLoginResponseDTO

--- BE/src/main/java/com/econovation/rere/domain/dto/response/UserLoginResponseDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/response/UserLoginResponseDTO.java
@@ -3,13 +3,17 @@ package com.econovation.rere.domain.dto.response;
 import lombok.*;
 
 //  로그인에 성공하면, SessionID와 맞는 닉네임을 전달한다.
-@Data
-@Builder
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class UserLoginResponseDTO {
 
     private String sessionId;
     private String nickname;
+
+    @Builder
+    public UserLoginResponseDTO(String sessionId, String nickname) {
+        this.sessionId = sessionId;
+        this.nickname = nickname;
+    }
 
 }
