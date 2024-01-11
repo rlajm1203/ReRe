c67d743f1c5d9140493b27ee0c641ed56757d92c
Kimjongmin <rlajm1203@naver.com>
2024-01-12 02:29:14 +0900

    refactor: add log print phrase

--- BE/src/main/java/com/econovation/rere/controller/ThemeController.java
+++ BE/src/main/java/com/econovation/rere/controller/ThemeController.java
@@ -4,6 +4,7 @@ import com.econovation.rere.apiresponse.ApiResult;
 import com.econovation.rere.apiresponse.ApiUtils;
 import com.econovation.rere.domain.dto.request.ThemeCreateRequestDTO;
 import com.econovation.rere.domain.dto.request.ThemeUpdateRequestDTO;
+import com.econovation.rere.domain.dto.response.ThemePageResponseDTO;
 import com.econovation.rere.domain.dto.response.ThemeResponseDTO;
 import com.econovation.rere.domain.entity.Theme;
 import com.econovation.rere.domain.entity.User;
@@ -31,26 +32,35 @@ public class ThemeController {
 
     //    새로운 목차 생성
     @PostMapping("/cardbook/{cardbookId}/theme/new")
-    public ApiResult<ThemeResponseDTO> createTheme(@RequestBody @Valid ThemeCreateRequestDTO themeCreateRequestDTO, @PathVariable("cardbookId") Integer cardbookId, HttpServletRequest request){
+    public ApiResult<Boolean> createTheme(@RequestBody @Valid ThemeCreateRequestDTO themeCreateRequestDTO, @PathVariable("cardbookId") Integer cardbookId, HttpServletRequest request){
         User user = (User)request.getSession().getAttribute("USER");
-        if(cardBookService.getCardbook(cardbookId).getWriter()!=user.getNickname()) throw new NotAthenticationException("카드북 작성자가 아닙니다.");
-
+        log.info("사용자 : "+user.getNickname()+", 목차 생성 : "+themeCreateRequestDTO.getName());
+        if(!cardBookService.getCardbook(cardbookId).getWriter().equals(user.getNickname())) throw new NotAthenticationException("카드북 작성자가 아닙니다.");
         return ApiUtils.success(themeService.register(themeCreateRequestDTO, cardbookId),"목차 생성에 성공하였습니다.");
     }
 
     //    해당 카드북의 모든 목차 가져오기
     @GetMapping("/cardbook/{cardbookId}/themes")
-    public ApiResult<List<ThemeResponseDTO>> themepageThemes(@PathVariable Integer cardbookId){
-        return ApiUtils.success(themeService.getAll(cardbookId),"목차 조회에 성공하였습니다.");
+    public ApiResult<ThemePageResponseDTO> themepageThemes(@PathVariable Integer cardbookId, HttpServletRequest request){
+        User user = (User)request.getSession().getAttribute("USER");
+        log.info("사용자 : "+user.getNickname()+", 목차 조회 (cardbookId): "+cardbookId);
+        ThemePageResponseDTO themePageResponseDTO = ThemePageResponseDTO.builder()
+                .themes(themeService.getAll(cardbookId, user.getUserId()))
+                .cardbookName(cardBookService.getCardbook(cardbookId).getName())
+                .build();
+        return ApiUtils.success(themePageResponseDTO,"목차 조회에 성공하였습니다.");
     }
 
     //    목차,카드 수정하기
     @PutMapping("/cardbook/{cardbookId}/theme/{themeId}")
-    public ApiResult<ThemeResponseDTO> updateThemeAndCards(@RequestBody @Valid ThemeUpdateRequestDTO themeUpdateRequestDTO, @PathVariable("cardbookId") Integer cardbookId, @PathVariable("themeId") Integer themeId, HttpServletRequest request){
+    public ApiResult<Boolean> updateThemeAndCards(@RequestBody @Valid ThemeUpdateRequestDTO themeUpdateRequestDTO, @PathVariable("cardbookId") Integer cardbookId, @PathVariable("themeId") Integer themeId, HttpServletRequest request){
         User user = (User)request.getSession().getAttribute("USER");
-        if(user.getNickname() != cardBookService.getCardbook(cardbookId).getWriter()) throw new NotAthenticationException("카드북 작성자가 아닙니다.");
-
+        log.info("사용자 : "+user.getNickname()+", 목차 수정 (themeId): "+themeId);
+        if(!cardBookService.getCardbook(cardbookId).getWriter().equals(user.getNickname())) throw new NotAthenticationException("카드북 작성자가 아닙니다.");
         return ApiUtils.success(themeService.update(themeUpdateRequestDTO, themeId),"목차 수정에 성공하였습니다.");
     }
+
+//    목차 삭제하기
+
 
 }
