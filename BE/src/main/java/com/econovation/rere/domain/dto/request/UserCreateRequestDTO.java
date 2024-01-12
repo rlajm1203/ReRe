7fdde8446bd1271578e186811b220a73640bb043
Kimjongmin <rlajm1203@naver.com>
2024-01-12 14:22:36 +0900

    refactor: remove @Size, @Pattern Annotation

--- BE/src/main/java/com/econovation/rere/domain/dto/request/UserCreateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/UserCreateRequestDTO.java
@@ -20,18 +20,18 @@ public class UserCreateRequestDTO {
 
     //  UserDTO 데이터를 받을 때는 UserId 값을 받지 않는다.
     @NotBlank(message ="Please write ID")
-    @Size(min = 6, max = 12, message = "ID는 6자 이상 12자 이하입니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9]+$", message = "알파벳 소문자 및 숫자만 사용할 수 있습니다.")
+//    @Size(min = 6, max = 12, message = "ID는 6자 이상 12자 이하입니다.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9]+$", message = "알파벳 소문자 및 숫자만 사용할 수 있습니다.")
     private String loginId;
 
     @NotBlank(message ="Please write PassWord")
-    @Size(min = 8, max = 15, message = "PW는 8자 이상 15자 이하입니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!~`<>,./?;:'\"\\[\\]{}()|_-])\\S*$", message = "알파벳 대소문자 및 숫자, 특수문자가 포함되어야 합니다.")
+//    @Size(min = 8, max = 15, message = "PW는 8자 이상 15자 이하입니다.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!~`<>,./?;:'\"\\[\\]{}()|_-])\\S*$", message = "알파벳 대소문자 및 숫자, 특수문자가 포함되어야 합니다.")
     private String pw;
 
     @NotBlank(message ="Please write NickName")
-    @Size(min = 2, max = 12, message = "닉네임은 2자 이상 12자 이하입니다.")
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "닉네임은 한글,알파벳 대소문자,숫자를 모두 사용할 수 있습니다.")
+//    @Size(min = 2, max = 12, message = "닉네임은 2자 이상 12자 이하입니다.")
+//    @Pattern(regexp = "^[가-힣a-zA-Z0-9]*$", message = "닉네임은 한글,알파벳 대소문자,숫자를 모두 사용할 수 있습니다.")
     private String nickname;
 
     @Builder
