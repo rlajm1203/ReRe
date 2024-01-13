2463be58ba1d6bf129f282b36f44a25c503926b0
Kimjongmin <rlajm1203@naver.com>
2024-01-13 12:36:38 +0900

    refactor: add OPTIONS method

--- BE/src/main/java/com/econovation/rere/config/WebConfig.java
+++ BE/src/main/java/com/econovation/rere/config/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.3:8080") //withCredential이 true일 경우 와일드카드 안됨, 사용자가 접속한 url 기준(프론트 서버가 로컬이면 localhost)
+                .allowedOrigins("http://localhost:5174") //withCredential이 true일 경우 와일드카드 안됨, 사용자가 접속한 url 기준(프론트 서버가 로컬이면 localhost)
 //                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
