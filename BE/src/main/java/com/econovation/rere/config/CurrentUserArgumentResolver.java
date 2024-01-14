6ae2fac2331b0b7265aef3dc7c711a8e6c39bd79
myeongheon <axm0219@naver.com>
2024-01-14 22:42:53 +0900

    feat:CurrentUser Annotation

---
+++ BE/src/main/java/com/econovation/rere/config/CurrentUserArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.econovation.rere.config;
+
+import com.econovation.rere.domain.entity.User;
+import com.econovation.rere.exception.NotAthenticationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        User user = (User)request.getSession().getAttribute("USER");
+        if (user == null) {
+            throw new NotAthenticationException("로그인이 필요한 서비스 입니다.");
+        }
+        return user.getUserId();
+    }
+}
