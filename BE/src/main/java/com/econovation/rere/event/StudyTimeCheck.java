17786b33013a69d3547ac37b81cac4bdfcd1e780
Kimjongmin <rlajm1203@naver.com>
2024-01-17 02:56:31 +0900

    refactor: using Spring Scheduler

--- BE/src/main/java/com/econovation/rere/event/StudyTimeCheck.java
+++ BE/src/main/java/com/econovation/rere/event/StudyTimeCheck.java
@@ -1,5 +1,6 @@
 package com.econovation.rere.event;
 
+import com.econovation.rere.Thread.StudyTimeCheckThread;
 import com.econovation.rere.domain.entity.CardBook;
 import com.econovation.rere.domain.entity.StudyComplete;
 import com.econovation.rere.domain.entity.User;
@@ -11,76 +12,51 @@ import com.econovation.rere.domain.repository.UserRepository;
 import com.econovation.rere.exception.CardBookNotFoundException;
 import com.econovation.rere.exception.UserCardBookNotFoundException;
 import com.econovation.rere.exception.UserNotFoundException;
+import com.econovation.rere.service.StudyService;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Builder
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class StudyTimeCheck extends Thread{
-
-    private UserRepository userRepository;
-    private CardBookRepository cardBookRepository;
-    private UserCardBookRepository userCardBookRepository;
-    private StudyCompleteRepository studyCompleteRepository;
-    private ApplicationEventPublisher publisher;
-    private Integer userId;
+@EnableAsync
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StudyTimeCheck{
+    private boolean userSchedule = false;
     private Integer cardbookId;
+    private Integer userId;
+    private final StudyTimeCheckThread studyTimeCheckThread;
+
+    public void setUserSchedule(boolean userSchedule) {
+        this.userSchedule = userSchedule;
+    }
+    public void setCardbookId(Integer cardbookId) {
+        this.cardbookId = cardbookId;
+    }
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Scheduled(cron = "* * * * * ?") // 매 시간 30분마다 수행
     public void run(){
 
-        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException());
-        CardBook cardBook = cardBookRepository.findById(cardbookId).orElseThrow(() -> new CardBookNotFoundException());
-        UserCardBook userCardBook = userCardBookRepository.findByCardbookAndUser(cardBook, user).orElseThrow(() -> new UserCardBookNotFoundException());
-
-        List<StudyComplete> studyCompletes = studyCompleteRepository.findAllByUserCardBook(userCardBook);
-
-        while(true) {
-            for (StudyComplete studyComplete : studyCompletes) {
-                LocalDateTime now = LocalDateTime.now();
-                LocalDateTime completeDate = studyComplete.getCompleteDate();
-
-                //
-                long differ_sec = (Timestamp.valueOf(now).getTime() - Timestamp.valueOf(completeDate).getTime()) / 1000;
-
-                if (differ_sec == 60 * 60 * 24 * 7)
-                    publisher.publishEvent(StudyAlarmEvent.builder()
-                            .message("일주일이 지났습니다. 학습해주세요.")
-                            .themeId(studyComplete.getTheme().getThemeId())
-                            .step(studyComplete.getStep())
-                            .build());
-
-                else if (differ_sec >= 60 * 60 * 24 * 3)
-                    publisher.publishEvent(StudyAlarmEvent.builder()
-                            .message("3일이 지났습니다. 학습해주세요.")
-                            .themeId(studyComplete.getTheme().getThemeId())
-                            .step(studyComplete.getStep())
-                            .build());
-
-                else if (differ_sec >= 60 * 60 * 24 * 1)
-                    publisher.publishEvent(StudyAlarmEvent.builder()
-                            .message("1일이 지났습니다. 학습해주세요.")
-                            .themeId(studyComplete.getTheme().getThemeId())
-                            .step(studyComplete.getStep())
-                            .build());
-
-                else if (differ_sec == 10)
-                    publisher.publishEvent(StudyAlarmEvent.builder()
-                            .message("10초가 지났습니다. 학습해주세요.")
-                            .themeId(studyComplete.getTheme().getThemeId())
-                            .step(studyComplete.getStep())
-                            .build());
+        try {
+            if (userSchedule) {
+                studyTimeCheckThread.setCardbookId(cardbookId);
+                studyTimeCheckThread.setUserId(userId);
+                studyTimeCheckThread.run();
             }
+        } catch (Exception e){
+            log.info(e.getMessage());
         }
     }
 
-    public void inputId(Integer cardbookId, Integer userId){
-        this.userId = userId;
-        this.cardbookId = cardbookId;
-    }
 }
