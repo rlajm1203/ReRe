72d0c5a01d22a421c906abb52f490832d00f47ae
Kimjongmin <rlajm1203@naver.com>
2024-01-17 02:54:21 +0900

    feat: add SSE Controller

--- BE/src/main/java/com/econovation/rere/controller/ServerSendEventController.java
+++ BE/src/main/java/com/econovation/rere/controller/ServerSendEventController.java
@@ -1,17 +1,20 @@
 package com.econovation.rere.controller;
 
-import com.econovation.rere.apiresponse.ApiResult;
-import com.econovation.rere.apiresponse.ApiUtils;
-import com.econovation.rere.domain.dto.response.StudyCompleteResponseDTO;
-import com.econovation.rere.domain.sse.SseEmitters;
-import com.econovation.rere.event.StudyAlarmEvent;
-import com.econovation.rere.service.StudyService;
+
+import com.econovation.rere.domain.entity.User;
+
+import com.econovation.rere.service.AlarmService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.parameters.P;
+
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,48 +22,15 @@ import java.io.IOException;
 @Slf4j
 public class ServerSendEventController {
 
-    private final SseEmitters sseEmitters;
-    private final StudyService studyService;
+    public static Map<Integer, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+    private final AlarmService alarmService;
 
     @GetMapping(value = "/connect", produces = "text/event-stream")
-    public SseEmitter connect() {
+    public SseEmitter connect(HttpServletRequest request) {
+        Integer userId = ((User)request.getSession().getAttribute("USER")).getUserId();
 //        emitter 생성, 추가
-        SseEmitter emitter = new SseEmitter();
-        sseEmitters.add(emitter);
-        try {
-            emitter.send(SseEmitter.event()
-                    .name("connect")
-                    .data("connected!"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        SseEmitter emitter = alarmService.connect(userId);
 
         return emitter;
     }
-
-    @GetMapping(value = "/studycheck")
-    public SseEmitter connect(StudyAlarmEvent event){
-        SseEmitter emitter = new SseEmitter();
-        sseEmitters.add(emitter);
-        try {
-            emitter.send(SseEmitter.event()
-                    .name("connect")
-                    .data("connected!"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return emitter;
-    }
-
-    @PostMapping(value = "/cardbook/{cardbookId}/themes/{userId}", produces = "text/event-stream")
-    public void check(@PathVariable("cardbookId")Integer cardbookId, @PathVariable("userId")Integer userId){
-
-        studyService.studyTimeCheck(cardbookId, userId);
-        for(int i=0; i<10; i++){
-            log.info("동시실행 되나?");
-        }
-
-    }
-
 }
