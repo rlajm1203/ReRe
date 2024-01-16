ddb4eb32f850208422e0646c761b5428d56430e6
Kimjongmin <rlajm1203@naver.com>
2024-01-17 02:55:44 +0900

    refactor: remove constructor

--- BE/src/main/java/com/econovation/rere/event/StudyAlarmEvent.java
+++ BE/src/main/java/com/econovation/rere/event/StudyAlarmEvent.java
@@ -9,12 +9,9 @@ import lombok.*;
 @Getter
 public class StudyAlarmEvent {
 
+    private Integer userId;
     private Integer themeId;
     private Integer step;
     private String message;
 
-    @Override
-    public String toString() {
-        return message+" 목차 : "+themeId + "단계 : " + step;
-    }
 }
