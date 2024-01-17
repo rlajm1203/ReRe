4ee4fc51d12389dfbda3542cab86b07f0ae6687b
myeongheon <axm0219@naver.com>
2024-01-18 02:00:50 +0900

    feat:Refactoring UserService

--- BE/src/main/java/com/econovation/rere/service/UserService.java
+++ BE/src/main/java/com/econovation/rere/service/UserService.java
@@ -12,53 +12,54 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void checkLoginId(UserLoginIdRequestDTO userLoginIdRequestDTO) throws DuplicateLoginIdException{
-        Optional<User> user = userRepository.findByLoginId(userLoginIdRequestDTO.getLoginId());
-        if (user.isPresent()) {
-            throw new DuplicateLoginIdException("중복된 아이디입니다.");
-        }
+    // 아이디 중복 체크
+    @Transactional(readOnly = true)
+    public void checkLoginId(UserLoginIdRequestDTO userLoginIdRequestDTO) {
+        userRepository.findByLoginId(userLoginIdRequestDTO.getLoginId()).ifPresent(
+                user -> { throw new DuplicateLoginIdException("중복된 아이디 입니다."); }
+        );
     }
 
-    public void checkNickname(UserNicknameRequestDTO userNicknameRequestDTO) throws DuplicateNicknameException{
-        Optional<User> user = userRepository.findByNickname(userNicknameRequestDTO.getNickname());
-        if (user.isPresent()) {
-            throw new DuplicateNicknameException("중복된 닉네임입니다.");
-        }
+    // 닉네임 중복 체크
+    @Transactional(readOnly = true)
+    public void checkNickname(UserNicknameRequestDTO userNicknameRequestDTO) {
+        userRepository.findByNickname(userNicknameRequestDTO.getNickname()).ifPresent(
+                user -> { throw new DuplicateNicknameException("중복된 닉네임 입니다."); }
+        );
     }
 
-    public String join(UserCreateRequestDTO userCreateRequestDTO) throws DuplicateLoginIdException{
+    // 회원가입
+    @Transactional
+    public String join(UserCreateRequestDTO userCreateRequestDTO) {
+        userRepository.findByLoginId(userCreateRequestDTO.getLoginId()).ifPresent(
+                user -> { throw new DuplicateLoginIdException("중복된 아이디 입니다.");});
+        userRepository.findByNickname(userCreateRequestDTO.getNickname()).ifPresent(
+                user -> { throw new DuplicateNicknameException("중복된 닉네임 입니다.");});
+
         User user = userCreateRequestDTO.toEntity();
-        if(userRepository.findByLoginId(userCreateRequestDTO.getLoginId()).isPresent()){
-            throw new DuplicateLoginIdException();
-        }
-        if(userRepository.findByNickname(userCreateRequestDTO.getNickname()).isPresent()){
-            throw new DuplicateNicknameException();
-        }
         user.updatePw(passwordEncoder.encode(user.getPw()));
         userRepository.save(user);
         return userCreateRequestDTO.getLoginId();
     }
 
 
-    public UserLoginResponseDTO login(UserLoginRequestDTO loginRequest, HttpSession session) throws InvalidLoginException, UserNotFoundException{
+    // 로그인
+    public UserLoginResponseDTO login(UserLoginRequestDTO loginRequest, HttpSession session) {
         User user = userRepository.findByLoginId(loginRequest.getLoginId())
                 .orElseThrow(() -> new UserNotFoundException("해당 사용자가 존재하지 않습니다."));
-        
+
         if (!passwordEncoder.matches(loginRequest.getPw(), user.getPw())) {
             throw new InvalidLoginException("아이디와 비밀번호가 일치하지 않습니다.");
         }
@@ -71,22 +72,33 @@ public class UserService {
                 .build();
     }
 
+    // 로그아웃
     public void logout(HttpSession session) {
         if (session != null) session.invalidate();
     }
 
+    // 비밀번호 변경
+    @Transactional
     public void updatePw(Integer userId, UserPwUpdateRequestDTO userPwUpdateRequestDTO) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new UserNotFoundException("해당 사용자가 존재하지 않습니다."));
+
         String pw = userPwUpdateRequestDTO.getPw();
         user.updatePw(passwordEncoder.encode(pw));
     }
 
+    // 닉네임 변경
+    @Transactional
     public void updateNickname(Integer userId, UserNicknameUpdateRequestDTO userNicknameUpdateRequestDTO) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new UserNotFoundException("해당 사용자가 존재하지 않습니다."));
+
         String nickname = userNicknameUpdateRequestDTO.getNickname();
-        log.info(nickname);
+
+        userRepository.findByNickname(nickname).ifPresent(
+                existingUser -> { throw new DuplicateNicknameException("중복된 닉네임 입니다."); }
+        );
+
         user.updateNickname(nickname);
     }
 }
