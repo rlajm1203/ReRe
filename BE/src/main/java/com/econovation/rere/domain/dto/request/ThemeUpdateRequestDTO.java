747bedef5a57223b1c1dc24c433ccf9db87e19b2
Kimjongmin <rlajm1203@naver.com>
2024-01-19 19:31:33 +0900

    fix: update code for test

--- BE/src/main/java/com/econovation/rere/domain/dto/request/ThemeUpdateRequestDTO.java
+++ BE/src/main/java/com/econovation/rere/domain/dto/request/ThemeUpdateRequestDTO.java
@@ -19,7 +19,7 @@ import java.util.List;
 @AllArgsConstructor
 public class ThemeUpdateRequestDTO {
 
-    @NotEmpty(message = "please write themeName")
+    @NotBlank(message = "please write themeName")
     private String name;
 
 //    @NotEmpty(message = "please write cardbookId")
@@ -32,7 +32,7 @@ public class ThemeUpdateRequestDTO {
 //    private LocalDateTime timenow;
 
     //    theme 하나에 여러 카드가 존재하므로
-    @NotEmpty(message = "please write cards")
+    @NotBlank(message = "please write cards")
     private List<CardUpdateRequestDTO> cards;
 
 }
